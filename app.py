c83291379dc00235e2872ab73cb56ea078664e6f
mstmarc <marcst@gmail.com>
2025-09-28 10:47:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -752,5 +752,5 @@ EQUIPO_EDIT_TEMPLATE = """
 """
 
 if __name__ == "__main__":
-    debug = os.environ.get("FLASK_DEBUG") == "1"
-    app.run(debug=debug)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
