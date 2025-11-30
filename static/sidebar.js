cff93bc99c664238d5b1964dc1cf6247528552e4
mstmarc <marcst@gmail.com>
2025-11-30 21:07:39 +0000

    Corrige agrupación de cartera en menú

--- static/sidebar.js
+++ static/sidebar.js
@@ -234,29 +234,27 @@
     menuHTML += `
                 <div class="sidebar-integrated-divider"></div>`;
 
-    // BLOQUE 3: INSPECCIONES (solo para admin)
-    if (tienePermiso('inspecciones', 'read')) {
+    // BLOQUE 3: INSPECCIONES, DEFECTOS, CARTERA E IMPORTACIÓN
+    if (tienePermiso('inspecciones', 'read') || tienePermiso('cartera', 'read')) {
         menuHTML += `
-                <!-- BLOQUE 3: INSPECCIONES -->
+                <!-- BLOQUE 3: INSPECCIONES, DEFECTOS, CARTERA E IMPORTACIÓN -->`;
+
+        if (tienePermiso('inspecciones', 'read')) {
+            menuHTML += `
                 <a href="/inspecciones" class="sidebar-integrated-link" style="padding-left: 54px;">
                     Inspecciones
                 </a>
                 <a href="/defectos_dashboard" class="sidebar-integrated-link" style="padding-left: 54px;">
                     Defectos
-                </a>
+                </a>`;
+        }
 
-                <div class="sidebar-integrated-divider"></div>`;
-    }
-
-    // BLOQUE 4: CARTERA Y ANÁLISIS
-    if (tienePermiso('cartera', 'read') || tienePermiso('inspecciones', 'read')) {
         menuHTML += `
-                <!-- BLOQUE 4: CARTERA Y ANÁLISIS -->
                 <a href="/cartera" class="sidebar-integrated-link" style="padding-left: 54px;">
-                    📊 Cartera y Análisis
+                    Cartera y Análisis
                 </a>
                 <a href="/cartera/importar" class="sidebar-integrated-link" style="padding-left: 54px;">
-                    ⬆️ Importar Datos
+                    Importar Datos
                 </a>
 
                 <div class="sidebar-integrated-divider"></div>`;
